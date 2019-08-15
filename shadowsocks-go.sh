9a0619930eb423a5a440d8d2cb090481681d7bac
charlie ethan <42068845+charlieethan@users.noreply.github.com>
2019-08-16 00:16:32 +0800

    Update shadowsocks-go.sh

--- shadowsocks-go.sh
+++ shadowsocks-go.sh
@@ -6,8 +6,6 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: Shadowsocks Golang
 #	Version: 1.0.0
-#	Author: Toyo
-#	Blog: https://doub.io/ss-jc67/
 #=================================================
 
 sh_ver="1.0.0"
